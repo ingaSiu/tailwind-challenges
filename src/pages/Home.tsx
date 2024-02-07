847c3b0119d401b5a1d304befee86ce515270339
ingaSiu <ingasiud@gmail.com>
2024-02-07 17:21:49 +0200

    create home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">Hello</h1>
+      <div>
+        <p>This will be the front page of the iCodeThis daily challenges web site</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
