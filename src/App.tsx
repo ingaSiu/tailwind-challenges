5164e513e797352832f4709e405acd4fca4dc894
ingaSiu <ingasiud@gmail.com>
2024-02-07 17:20:08 +0200

    import routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
+import Routes from './routes/Routes';
+
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello</h1>
-      <div>
-        <p>This will be the front page of the iCodeThis daily challenges web site</p>
-      </div>
+      <Routes />
     </>
   );
 };
 
 export default App;
-
