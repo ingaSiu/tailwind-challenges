77ead480b028798f5f86ffe2724547d1b5f3bb6b
ingaSiu <ingasiud@gmail.com>
2024-02-08 18:21:27 +0200

    fix

--- src/pages/Challenge8.tsx
+++ src/pages/Challenge8.tsx
@@ -1,16 +1,16 @@
 const Challenge8 = () => {
   return (
     <div>
-      <div class="flex flex-col gap-2 justify-end mt-5 rounded-sm">
-        <div class="flex justify-end">
-          <div class="bg-white cursor-pointer w-[50px] h-[50px] flex items-center justify-center">
+      <div className="flex flex-col gap-2 justify-end mt-5 rounded-sm">
+        <div className="flex justify-end">
+          <div className="bg-white cursor-pointer w-[50px] h-[50px] flex items-center justify-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="w-10 h-10"
+              className="w-10 h-10"
             >
               <path
                 stroke-linecap="round"
@@ -21,35 +21,35 @@ const Challenge8 = () => {
           </div>
         </div>
 
-        <div class="bg-white rounded-sm py-4 px-3 shadow-md shadow-amber-600/30">
-          <ul class="">
-            <li class="p-3 text-orange-800 hover:bg-amber-200">Edit your list</li>
-            <li class="p-3 text-orange-800 hover:bg-amber-200">Account settings</li>
-            <li class="p-3 text-orange-800 hover:bg-amber-200"> View your profile</li>
+        <div className="bg-white rounded-sm py-4 px-3 shadow-md shadow-amber-600/30">
+          <ul className="">
+            <li className="p-3 text-orange-800 hover:bg-amber-200">Edit your list</li>
+            <li className="p-3 text-orange-800 hover:bg-amber-200">Account settings</li>
+            <li className="p-3 text-orange-800 hover:bg-amber-200"> View your profile</li>
           </ul>
         </div>
       </div>
 
-      <div class="py-12 pl-12 pr-16 rounded-lg bg-white shadow-md shadow-amber-600/30 ">
-        <h1 class="text-2xl font-semibold mb-4">Customize your widget</h1>
+      <div className="py-12 pl-12 pr-16 rounded-lg bg-white shadow-md shadow-amber-600/30 ">
+        <h1 className="text-2xl font-semibold mb-4">Customize your widget</h1>
 
-        <div class="mb-6">
-          <div class="flex items-center gap-2 mb-3">
+        <div className="mb-6">
+          <div className="flex items-center gap-2 mb-3">
             <input
               type="radio"
-              class="appearance-none rounded-full w-[16px] h-[16px] border border-amber-800 checked:outline-2 checked:outline checked:outline-amber-600 checked:bg-orange-600"
+              className="appearance-none rounded-full w-[16px] h-[16px] border border-amber-800 checked:outline-2 checked:outline checked:outline-amber-600 checked:bg-orange-600"
             />
-            <label for="" class="text-amber-800">
+            <label for="" className="text-amber-800">
               Display Contact Button
             </label>
           </div>
 
-          <div class="mb-3">
+          <div className="mb-3">
             <input
               type="radio"
-              class="appearance-none rounded-full w-[16px] h-[16px] border border-amber-800 checked:outline-2 checked:outline checked:outline-amber-600 checked:bg-orange-600"
+              className="appearance-none rounded-full w-[16px] h-[16px] border border-amber-800 checked:outline-2 checked:outline checked:outline-amber-600 checked:bg-orange-600"
             />
-            <label for="" class="text-amber-800">
+            <label for="" className="text-amber-800">
               Hide email address
             </label>
           </div>
@@ -57,39 +57,39 @@ const Challenge8 = () => {
           <div>
             <input
               type="radio"
-              class="appearance-none rounded-full w-[16px] h-[16px] border border-amber-800 checked:outline-2 checked:outline checked:outline-amber-600 checked:bg-orange-600"
+              className="appearance-none rounded-full w-[16px] h-[16px] border border-amber-800 checked:outline-2 checked:outline checked:outline-amber-600 checked:bg-orange-600"
             />
-            <label for="" class="text-amber-800">
+            <label for="" className="text-amber-800">
               Display Publish button
             </label>
           </div>
         </div>
 
-        <div class="flex gap-3 ">
-          <button class="py-3 px-8 w-full rounded-lg  bg-slate-100 hover:bg-slate-200">Cancel</button>
-          <button class="py-3 px-8 w-full rounded-lg  bg-amber-400 hover:bg-amber-200">Save</button>
+        <div className="flex gap-3 ">
+          <button className="py-3 px-8 w-full rounded-lg  bg-slate-100 hover:bg-slate-200">Cancel</button>
+          <button className="py-3 px-8 w-full rounded-lg  bg-amber-400 hover:bg-amber-200">Save</button>
         </div>
       </div>
 
-      <div class="py-10 pl-10 pr-16 mb-5 rounded-lg bg-white shadow-md shadow-amber-600/30">
-        <h1 class="text-2xl font-semibold mb-4 text-orange-800">Other</h1>
+      <div className="py-10 pl-10 pr-16 mb-5 rounded-lg bg-white shadow-md shadow-amber-600/30">
+        <h1 className="text-2xl font-semibold mb-4 text-orange-800">Other</h1>
 
-        <div class="mb-4 flex md:items-center gap-2 flex-col md:flex-row">
+        <div className="mb-4 flex md:items-center gap-2 flex-col md:flex-row">
           <input
             type="checkbox"
-            class="appearance-none w-[20px] h-[20px]  border border-slate-400 rounded-sm checked:bg-orange-900 checked:outline-double checked:outline-amber-500 checked:border-none checked:outline-4"
+            className="appearance-none w-[20px] h-[20px]  border border-slate-400 rounded-sm checked:bg-orange-900 checked:outline-double checked:outline-amber-500 checked:border-none checked:outline-4"
           />
-          <label for="" class="text-amber-800">
+          <label for="" className="text-amber-800">
             Turn on in-app notifications.
           </label>
         </div>
 
-        <div class="mb-4 flex md:items-center gap-2 flex-col md:flex-row">
+        <div className="mb-4 flex md:items-center gap-2 flex-col md:flex-row">
           <input
             type="checkbox"
-            class="appearance-none w-[20px] h-[20px]  border border-slate-400 rounded-sm checked:bg-orange-900 checked:outline-double checked:outline-amber-500 checked:border-none checked:outline-4"
+            className="appearance-none w-[20px] h-[20px]  border border-slate-400 rounded-sm checked:bg-orange-900 checked:outline-double checked:outline-amber-500 checked:border-none checked:outline-4"
           />
-          <label for="" class="text-amber-800">
+          <label for="" className="text-amber-800">
             Receive weekly newsletters and other offers.
           </label>
         </div>
