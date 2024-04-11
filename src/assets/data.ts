d728dab4b63d9c38aa51dc6c183c2f9e4891f6e7
ingaSiu <ingasiud@gmail.com>
2024-04-11 15:21:07 +0300

    create static data file for page

---
+++ src/assets/data.ts
@@ -0,0 +1,8 @@
+export const data = [
+  {
+    title: 'Challenge 1',
+    image: 'https://i.imgur.com/g8vUPGW.png',
+    path: 'challenge1',
+    url: 'https://icodethis.com/modes/design-to-code/200/submissions/280717',
+  },
+];
