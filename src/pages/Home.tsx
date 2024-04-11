dffac62cd61ec0d4d00e1bcd11305c2c4b32d5aa
ingaSiu <ingasiud@gmail.com>
2024-04-11 15:21:38 +0300

    map data with card component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,6 @@
+import ChallengeCard from '../components/ChallengeCard';
+import { data } from '../assets/data';
+
 const Home = () => {
   return (
     <>
@@ -5,6 +8,9 @@ const Home = () => {
       <div>
         <p>This will be the front page of the iCodeThis daily challenges web site</p>
       </div>
+      {data.map((card) => (
+        <ChallengeCard key={card.path} title={card.title} image={card.image} path={card.path} url={card.url} />
+      ))}
     </>
   );
 };
