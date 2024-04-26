c8e585f1cc0b6cec17ace3a958386e64f4ef0bba
ingaSiu <ingasiud@gmail.com>
2024-04-26 09:45:09 +0300

    change card styling, title

--- src/components/ChallengeCard.tsx
+++ src/components/ChallengeCard.tsx
@@ -1,3 +1,4 @@
+import Button from './Button';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,16 +13,23 @@ const ChallengeCard = ({ image, title, path, url }: CardProps) => {
   const navigate = useNavigate();
 
   return (
-    <div className="border w-[550px]">
-      <img src={image} alt="project" />
+    <div className="border rounded-xl shadow-lg overflow-hidden ">
+      <div className="relative w-full h-auto aspect-video">
+        <img className="w-fill" src={image} alt="project" />
+      </div>
 
-      <div>
-        <h1 className="text-xl text-center">{title}</h1>
+      <div className="px-5 pb-10 ">
+        <h1 className="text-2xl text-center pt-4">
+          Challenge -{' '}
+          <span className="font-semibold bg-gradient-to-r from-pink-400 to-violet-400 inline-block text-transparent bg-clip-text">
+            {title}
+          </span>
+        </h1>
 
-        <div className="flex gap-2 justify-between">
-          <button onClick={() => navigate(path)}>View Task</button>
+        <div className="flex gap-8 justify-center mt-4">
+          <Button onClick={() => navigate(path)}>View Task</Button>
           <Link to={url} target="_blank">
-            <button>iCodeThis challenge</button>
+            <Button>iCodeThis challenge</Button>
           </Link>
         </div>
       </div>
