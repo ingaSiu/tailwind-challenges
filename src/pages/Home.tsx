29cbf5672e2107f131c16d6e37414c702c2b982b
ingaSiu <ingasiud@gmail.com>
2024-04-26 09:43:37 +0300

    add simple grid for cards display

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,9 +8,12 @@ const Home = () => {
       <div>
         <p>This will be the front page of the iCodeThis daily challenges web site</p>
       </div>
-      {data.map((card) => (
-        <ChallengeCard key={card.path} title={card.title} image={card.image} path={card.path} url={card.url} />
-      ))}
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-10">
+        {data.map((card) => (
+          <ChallengeCard key={card.path} title={card.title} image={card.image} path={card.path} url={card.url} />
+        ))}
+      </div>
     </>
   );
 };
